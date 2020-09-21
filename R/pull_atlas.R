d12241d701b1ee00401e65c823cd6201350f30e3
Matias Andina <matiasandina@gmail.com>
2020-09-21 09:12:26 -0400

    couldn't figure out what's wrong with match.call() asked on stackoverflow. Implemented a hotfix to be able to keep using it

--- R/pull_atlas.R
+++ R/pull_atlas.R
@@ -15,11 +15,20 @@
 pull_atlas <- function(AP, xpos = 0, x = 0, adj = c( -1, 0),
                        cex = 1.5, width=NULL, save=FALSE,
                        new_device=TRUE, force_redraw = FALSE){
+
+  # hotfix -- I couldn't figure out the fun_call$AP problem
+  # see here
+  # https://stackoverflow.com/questions/63980673/using-match-call-in-recursive-function
+
+  if(AP < -5.5){
+    return("We cannot display AP < -5.5")
+  }
+
   # catch the function call
-  fun_call <- match.call.defaults()
+  fun_call <- SMART:::match.call.defaults()
+
   window_title <- paste0("Plate ", toString(platereturn(AP)),
                          ", AP ", toString(round(roundAP(AP), digits=2)))
-
   # no width makes error o  n Ubuntu systems
   if(is.null(width) && get_os() == "linux"){
     width <- 10
@@ -48,8 +57,9 @@ pull_atlas <- function(AP, xpos = 0, x = 0, adj = c( -1, 0),
       plot(imager::load.image(path_to_try), axes=FALSE)
 
     } else {
+      print("File does not exist calling with force_redraw")
       # else, call the function again forcing redraw
-      pull_atlas(fun_call$AP, xpos=eval(fun_call$xpos),  fun_call$x,
+      pull_atlas(AP = fun_call$AP, xpos=eval(fun_call$xpos),  fun_call$x,
                  fun_call$adj, fun_call$cex,
                  fun_call$width, fun_call$save,
                  fun_call$new_device, force_redraw = TRUE)
@@ -57,13 +67,38 @@ pull_atlas <- function(AP, xpos = 0, x = 0, adj = c( -1, 0),
 
 
   } else {
+    print("enter else")
+    # construct available options
+    available_drawing_options <- tibble(
+      plate.id = atlasIndex$plate.id,
+      mm.from.bregma = atlasIndex$mm.from.bregma,
+      plane = atlasIndex$plane
+    ) %>%
+      # TODO: Fix here if you want both planes
+      filter(plane == "coronal") %>%
+      mutate(plate = platereturn(mm.from.bregma),
+             # ! NULL will give TRUE if exists
+             coronal_plate_exists = !sapply(EPSatlas$plate.info, is.null))
+    print("enter tryCatch")
 
-    # re-draw
-    wholebrain::schematic.plot(dataset = NULL, mm.grid=F, coordinate = roundAP(AP), region.colors =  TRUE, device = F)
-    # this eval is just in case the adj gets passed as c(...)
-    text(x = x, window_title, adj = eval(adj), cex = cex)
+    tryCatch({
+      # try re-draw
+      wholebrain::schematic.plot(dataset = NULL, mm.grid=F, coordinate = roundAP(AP), region.colors =  TRUE, device = F)
+      # this eval is just in case the adj gets passed as c(...)
+      text(x = x, window_title, adj = eval(adj), cex = cex)
 
-    # if(save){
+    }, error = function(e) {
+      message("These are the AP levels we can draw:")
+      message("----------------------------")
+      print(
+      available_drawing_options %>%
+        filter(coronal_plate_exists == TRUE) %>%
+        pull(mm.from.bregma)
+      )
+      message("----------------------------")
+      message(paste("We can't draw the AP level you gave (AP =", fun_call$AP, ")"))
+    })
+
     # save plot here ? could be useful to people just wanting to get atlas plates ?
     # }
 
